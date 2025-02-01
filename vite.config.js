6e7f973ebca9a36cff7ab12283d22db47646e999
MURAMASA2470 <keisuke226214@gmail.com>
2025-02-01 23:56:53 +0900

    tailwindcssを導入

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig({
         }),
     ],
     server: {
-      host: process.env.HMR_HOST || "127.0.0.1",
+      host: process.env.VITE_HMR_HOST || "127.0.0.1",
       port: process.env.VITE_PORT || 5173,
       hmr: {
-          host: process.env.HMR_HOST || "127.0.0.1",
+          host: process.env.VITE_HMR_HOST || "127.0.0.1",
           port: process.env.VITE_PORT || 5173,
       },
-  },
+    },
 });
